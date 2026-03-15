922e6b7d86ca746f6636c6736b2889fc95eaded5
Odin Kammerloher <kammerloher.andreas@gmail.com>
2026-03-15 14:38:50 +0100

    Fix 'STATUS_ENTRYPOINT_NOT_FOUND' by removing windows manifest from build

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,16 +1,4 @@
+// If you ever get "STATUS_ENTRYPOINT_NOT_FOUND", it is likely because we do some weird stuff with the build in here
 fn main() {
-    let windows_attributes = tauri_build::WindowsAttributes::new().app_manifest(
-        r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-    <security>
-      <requestedPrivileges>
-        <requestedExecutionLevel level="asInvoker" uiAccess="false"/>
-      </requestedPrivileges>
-    </security>
-  </trustInfo>
-</assembly>"#,
-    );
-    let attributes = tauri_build::Attributes::new().windows_attributes(windows_attributes);
-    tauri_build::try_build(attributes).expect("failed to run tauri-build");
+    tauri_build::build()
 }
